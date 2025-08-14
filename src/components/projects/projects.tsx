b5e06c961580eaa38c3188aa96f027b66bedda3e
Ксения Шлёнская <k.shlenskaya@duotek.ru>
2025-08-14 17:47:53 +0300

    Update Projects section

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -2,6 +2,8 @@ import styles from "./projects.module.scss";
 
 import Media from "../_block/media/media";
 
+import fav from '../../assets/images/fav.png';
+
 import lsm from "../../assets/images/projects/lsm.png";
 import epl from "../../assets/images/projects/epl.png";
 import ihe from "../../assets/images/projects/ihe.png";
@@ -24,16 +26,16 @@ const Projects = () => {
 			link: `https://id.world/en`,
 		},
 		{
-			image: lsm,
-			title: "Legio Sound & Music",
-			description: `Legio Sound & Music is a platform for licensing and streaming high-quality audio content designed for film, advertising, and media projects. It features user accounts with subscription options, playlist creation, and license management. A complex multi-level filtering system supports efficient content discovery. Audio is streamed seamlessly across devices without requiring full file downloads.`,
-			link: `https://legiosoundmusic.ru/en`,
-		},
-		{
 			image: auth,
 			title: '@duotek/nuxt3-auth',
 			description: `@duotek/nuxt3-auth is a custom authentication module for Nuxt 3, developed to address the lack of reliable built-in solutions. It supports configurable login and registration strategies, secure token storage via cookies, user data handling, and redirect control. Includes a dedicated $authFetch extension for authorized API requests. Is used across all new internal and client-facing projects.`,
 			link: `https://www.npmjs.com/package/@duotek/nuxt3-auth`,
+		},
+		{
+			image: lsm,
+			title: "Legio Sound & Music",
+			description: `Legio Sound & Music is a platform for licensing and streaming high-quality audio content designed for film, advertising, and media projects. It features user accounts with subscription options, playlist creation, and license management. A complex multi-level filtering system supports efficient content discovery. Audio is streamed seamlessly across devices without requiring full file downloads.`,
+			link: `https://legiosoundmusic.ru/en`,
 		},
 		{
 			image: epl,
@@ -41,12 +43,13 @@ const Projects = () => {
 			description: `Explat is a digital platform for international trade services. The system enables full-cycle interaction between clients and service providers, including service ordering, verification, and payment. It features dedicated user dashboards for both clients and suppliers, integrated chat for communication, and an admin panel for platform management. The architecture is designed for scalability and secure transaction processing.`,
 			link: `https://explat.kz/`,
 		},
-		{
-			image: ihe,
-			title: "iHels",
-			description: `A full-featured online store was developed with a product catalog, advanced filtering, product pages, shopping cart, promo code system, and online payments. The service supports the entire user journey from product selection to checkout and is designed for scalability and integration with external systems such as CRM and logistics. An admin panel and a supplier dashboard were also implemented for managing listings, orders, and analytics.`,
-			link: `https://ihels.shop/`,
-		},
+		
+		// {
+		// 	image: ihe,
+		// 	title: "iHels",
+		// 	description: `A full-featured online store was developed with a product catalog, advanced filtering, product pages, shopping cart, promo code system, and online payments. The service supports the entire user journey from product selection to checkout and is designed for scalability and integration with external systems such as CRM and logistics. An admin panel and a supplier dashboard were also implemented for managing listings, orders, and analytics.`,
+		// 	link: `https://ihels.shop/`,
+		// },
 	];
 
 	return (
@@ -54,6 +57,7 @@ const Projects = () => {
 			<div className={`${styles["projects__container"]} layout`}>
 				<div className={styles["projects__header"]}>
 					<h2 className={styles["projects__title"]}>
+						<img src={fav} alt="Manually written word" className={styles["projects__title-image"]} />
 						<span className="text-display">My </span>{" "}
 						<span className="text-display text-w-800">
 							Projects
